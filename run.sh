edccdf4ddc31189d80cc342b303a234e737ea8a8
saveffer1 <saveffer01@gmail.com>
2023-03-26 21:59:39 +0700

    add lab 4, 5 and rewrite run.sh to support gcc run

--- run.sh
+++ run.sh
@@ -3,15 +3,39 @@
 # Read input from user for folder name
 read -p "Enter folder name: " folder_name
 
-# Change to the input folder
+if [ ! -d "$folder_name" ]; then
+    echo "No such file or directory."
+    exit 0
+fi
+
 cd "$folder_name"
 
-# Assemble myProg.s to create myProg.o
-as -o program.o program.s
+cr=`echo $'\n.'`
+cr=${cr%.}
 
-# Link myProg.o to create Program
-ld -o program program.o
+while true; do
+    read -p " 1: asm $cr 2: gcc $cr 3: exit $cr>> " choice
 
+    case $choice in
+    1)
+        as -o program.o program.s
+        ld -o program program.o
+        break
+        ;;
+    2)
+        as -g -o program.o program.s
+        gcc -o program program.o
+        break
+        ;;
+    3)
+        echo "kuy!"
+        exit 0
+        ;;
+    *)
+        echo "Invalid choice"
+        ;;
+    esac
+done
 # Run the resulting executable
 # ./program ; echo $?
 ./program
